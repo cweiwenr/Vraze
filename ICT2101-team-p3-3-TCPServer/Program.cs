38dab33af1cbf36b3943617fde903fb95419c01b
cweiwenr <reubenchongww2512@gmail.com>
2021-11-12 13:51:18 +0800

    tcp server

---
+++ ICT2101-team-p3-3-TCPServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread t = new Thread(delegate ()
+        {
+            // change ip to ur com's private ip
+            //Server myserver = new Server("192.168.157.22", 5000);
+            Server myserver = new Server("172.20.10.2", 5000);
+        
+        });
+        t.Start();
+        
+        Console.WriteLine("Server Started...!");
+    }
+}
